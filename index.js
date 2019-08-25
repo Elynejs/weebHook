20e12174eba0ef994f5dcca98cfdfe1e96b52c06
Elynejs <matthias.gomes03@gmail.com>
2019-08-25 16:03:15 +0200

    scrapped manga titles from last release

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const rp = require('request-promise');
 // const Discord = require('discord.js');
 // const client = Discord.Client();
 const options = {
-    url: `https://mangakakalot.com/page`,
+    url: `https://mangakakalot.com/latest`,
     transform: function(body) {
         return ch.load(body);
     }
 };
 
-const junk = ['HOME','LATEST MANGA','HOT MANGA','NEW MANGA','COMPLETED MANGA','MANGAKAKALOT','Chapter'];
 let rawStr;
 let str = [];
-let mangas = [];
+// let mangas = [];
 
 rp(options)
     .then((body) => {
-        body('li').each((i, elem) => {
+        body('.list-truyen-item-wrap').children('h3').each((i, elem) => {
             rawStr = body(elem).text();
-            str[i] = rawStr.replace(/[\n\r]/g, ' ').trim();
+            str.push(rawStr.replace(/[\n\r]/g, ' ').trim());
             console.log(str[i]);
             }
         )}
@@ -26,20 +25,3 @@ rp(options)
     .catch((err) => {
         console.log('HTTP Request encountered an error. This is likely due to a server maintenance.' + err);
     });
-
-const filter = () => {
-    let i;
-    let j;
-    for (i = 0; i <= str.length; i++) {
-        for (j = 0; j <= junk.length; j++) {
-            if (str[i].includes(junk[j])) {
-                console.log('Removed a string because it contained a junk');
-            } else {
-                mangas[i] = str;
-            }
-        }
-    }
-};
-
-filter();
-console.log(mangas);

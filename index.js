84a75ee19bf31b4f0b47d1b712fa13f5cf1c45cf
Elynejs <matthias.gomes03@gmail.com>
2019-08-25 15:17:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,37 @@ const options = {
     }
 };
 
-let str;
+const junk = ['HOME','LATEST MANGA','HOT MANGA','NEW MANGA','COMPLETED MANGA','MANGAKAKALOT','Chapter'];
+let rawStr;
+let str = [];
 let mangas = [];
 
 rp(options)
     .then((body) => {
         body('li').each((i, elem) => {
-            str = body(elem).text();
-            mangas[i] = str.replace(/[\n\r]/g, ' ').trim();
-            console.log(mangas);
-        });
-    })
+            rawStr = body(elem).text();
+            str[i] = rawStr.replace(/[\n\r]/g, ' ').trim();
+            console.log(str[i]);
+            }
+        )}
+        )
     .catch((err) => {
         console.log('HTTP Request encountered an error. This is likely due to a server maintenance.' + err);
     });
+
+const filter = () => {
+    let i;
+    let j;
+    for (i = 0; i <= str.length; i++) {
+        for (j = 0; j <= junk.length; j++) {
+            if (str[i].includes(junk[j])) {
+                console.log('Removed a string because it contained a junk');
+            } else {
+                mangas[i] = str;
+            }
+        }
+    }
+};
+
+filter();
+console.log(mangas);

8297b8af8e50e3c6fcd9cc5a6283a895488fc586
Elynejs <matthias.gomes03@gmail.com>
2019-08-25 17:00:44 +0200

    added commands

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const ch = require('cheerio');
 const rp = require('request-promise');
-// const Discord = require('discord.js');
-// const client = Discord.Client();
+const Discord = require('discord.js');
+const client = Discord.Client();
+const token = require('token.json');
 const options = {
     url: `https://mangakakalot.com/latest`,
     transform: function(body) {
@@ -11,17 +12,56 @@ const options = {
 
 let rawStr;
 let str = [];
-// let mangas = [];
+let track = [];
+let released = [];
 
-rp(options)
-    .then((body) => {
-        body('.list-truyen-item-wrap').children('h3').each((i, elem) => {
-            rawStr = body(elem).text();
-            str.push(rawStr.replace(/[\n\r]/g, ' ').trim());
-            console.log(str[i]);
-            }
-        )}
-        )
-    .catch((err) => {
-        console.log('HTTP Request encountered an error. This is likely due to a server maintenance.' + err);
-    });
+const scrap = () => {
+    rp(options)
+        .then((body) => {
+            body('.list-truyen-item-wrap').children('h3').each((i, elem) => {
+                rawStr = body(elem).text();
+                str.push(rawStr.replace(/[\n\r]/g, ' ').trim());
+                }
+            )}
+            )
+        .catch((err) => {
+            console.log('HTTP Request encountered an error. This is likely due to a server maintenance.' + err);
+        });
+    };
+
+const check = () => {
+    let i;
+    for (i = 0; i <= str.length; i++) {
+        if (str[i] in track) {
+            released.push(str);
+        }
+    }
+}
+
+setInterval(scrap, 1000 * 60 * 60);
+setInterval(check, 1000 * 60 * 60);
+
+client.on('Message', msg => {
+    if (msg.author.bot) return;
+    if (msg.content.indexOf(token.prefix) !== 0) return;
+    const args = msg.content.slice(token.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (command === 'add') {
+        if (args[0]) {
+            track.push(args[0]);
+        } else {
+            msg.channel.send('Please specify the name of the manga you want to add to your tracking.'+
+            '\nNote that this is case sensitive so just copy/paste it from the site.');
+        }
+    } else if (command === 'list') {
+        let i;
+        for (i = 0; i <= track.length; i++) {
+            msg.channel.send(track[i]);
+        }
+    } else {
+        msg.channel.send('You failed to type a recognized command');
+    }
+})
+
+
+client.login(token);
